922d7cd9db6933da9342517f01036c48362f657d
MausoofAbdullah <mausoofabdullah@gmail.com>
2023-02-07 10:37:34 +0530

    api edited

--- src/api/Authrequest.js
+++ src/api/Authrequest.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-//const API= axios.create({baseURL:process.env.REACT_API_CALL})
-const API= axios.create({baseURL:'http://localhost:5000'})
+const API= axios.create({baseURL:process.env.REACT_API_CALL})
+//const API= axios.create({baseURL:'http://localhost:5000'})
 
 export const logIn=(FormData)=>API.post("/auth/login",FormData)
 export const signup=(FormData)=>API.post("/auth/register",FormData)
